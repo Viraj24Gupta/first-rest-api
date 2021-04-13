1995b7867281c612a6e2f33a3a7991b8209128d2
Viraj Gupta <61087588+Viraj24Gupta@users.noreply.github.com>
2021-04-14 00:58:07 +0530

    Update new.js

--- routes/new.js
+++ routes/new.js
@@ -1,10 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const dbejs = require('../models/db_schema');
+
 
 //get new.ejs on home page
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res)=> {
     res.render('new',{title: 'first rest api'});
+});
+
+
+//retrieve data
+router.post('/', async(req,res)=>{
+    await dbejs.create({
+        title: req.body.title,
+        description: req.body.description
+    } , (err,data)=>{
+        if(err)
+            console.log(err);
+        else
+            console.log(data);
+    });
 });
 
 module.exports = router;
